d33778ccd4ee4314f1c4b87e836f88e505b9e181
shri <lilredindy@github.com>
2015-09-01 14:19:23 -0400

    notes

--- python/elephant.py
+++ python/elephant.py
@@ -2,10 +2,10 @@ import requests
 
 
 
-for i in range(0, 0):
+for i in range(0, 100):
 	r = requests.get("http://www.theguardian.com/environment/2015/jul/07/zimbabwe-activists-deplore-sale-of-24-elephant-calves-to-china#comment-55235133")
 	print r.status_code
 
 
 import socket
-print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1])
+print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1]) 
