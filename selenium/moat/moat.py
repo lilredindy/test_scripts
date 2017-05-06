49c95dabfbeaa93891c01d03c6debfff528bc315
Shri Amin <shri.amin@gmail.com>
2017-05-06 09:31:38 -0400

    stuff

--- selenium/moat/moat.py
+++ selenium/moat/moat.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-import time, traceback
+import sys, time, traceback, random, re
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, ElementNotVisibleException
@@ -24,27 +24,28 @@ more than 100 ads in the result set.
 """
 
 import os
-GECKO = ':/Users/shriamin/Development/test_tools/selenium'
+GECKO = 'C:\\Users\\shri amin\\Desktop\\shri\\Development\\test_tools'
 os.environ['PATH'] += GECKO
 print os.environ['PATH']
 
+
 class moat(unittest.TestCase):
 
-	def setUp(self): 
-		#non-cloud code
-		#chrome_path = "C:\\Documents and Settings\\chuck\Desktop\\shri\\Development\\test_tools\\selenium\\chromedriver.exe"
-		self.driver = webdriver.Chrome() 
-		#self.driver = webdriver.Firefox()
-		#self.lib = moatLib(self.driver)  #library of useful methods
-		self.driver.implicitly_wait(10) #waits for element to appear in DOM
-		self.driver.set_page_load_timeout(10) #waits for page to fully load (readyState = true), but AJAX not included
-		self.driver.get("http://www.moat.com/")
-		
+	def setUp(self):
+		try: 
+			#non-cloud code
+			#self.driver = webdriver.Chrome()
+			self.driver = webdriver.Firefox()
+			self.driver.implicitly_wait(5) #waits for element to appear in DOM
+			self.driver.set_page_load_timeout(3) #waits for page to fully load (readyState = true), but AJAX not included
+			self.driver.get("http://www.moat.com/")
+		except Exception as e:
+			self.driver.save_screenshot(os.getcwd() + os.path.sep + unittest.TestCase.id(self).split(".")[2] + ".png") 			
+			self.fail(e)
+
 
 	def tearDown(self): 
-		pass
-		#self.driver.quit() 
-		#self.driver.close() #only works with remote webdriver
+		self.driver.quit() 
 
 	@classmethod
 	def setUpClass(moat):
@@ -54,94 +55,136 @@ class moat(unittest.TestCase):
 	def tearDownClass(moat):
 		pass
 
-	@unittest.skip("")
+	#@unittest.skip("")
 	def test_scenario1(self) :
-		#create python list of ad links
-		L = []
-		count = 3
-		while (count > 0):
-			self.driver.get("https://www.moat.com/")
-			#self.driver.refresh()
-			
-			brands = [brand for brand in self.driver.find_element_by_css_selector("span.random-brand.hidden-xs").find_elements_by_tag_name("a")]		
-			
-			#STEP1: make sure of randomness
-			for brand in brands:
-				#self.assertFalse(list(set(X).intersection(set(e.text))))        
-				self.assertTrue(brand.text not in L)
-				L.append(str(brand.text)) #append new elements to master list
-			print (L)
- 
-			#STEP2: validate the link works
-			ad_text = brands[0].text
-			brands[0].click() 
-			self.assertTrue(self.driver.find_element_by_css_selector("span.page-type"))
-			self.assertTrue(ad_text in self.driver.title)
-			#self.assertTrue(ad_text in self.driver.page_source) 
-			count -= 1 #decrement
+		"""1. Verify that the “Try These” links are random and that they work."""
+		try:
+			L = [] 
+			count = 3
+			while (count > 0):
+				self.driver.get("https://www.moat.com/")
+				
+				brands = [brand for brand in self.driver.find_element_by_css_selector("span.random-brand.hidden-xs").find_elements_by_tag_name("a")]		
+				
+				#STEP1: make sure of randomness
+				for brand in brands:
+					#self.assertFalse(list(set(X).intersection(set(e.text))))        
+					self.assertTrue(brand.text not in L)
+					L.append(str(brand.text)) #append new elements to master list
+				print (L)
+	 
+				#STEP2: validate the link works
+				brand_name = brands[0].text
+				brands[0].click() 
+				bold_label = self.driver.find_element_by_css_selector("span.page-type")
+				time.sleep(3) #needs a wait_for_AJAX or something like that, EC class has nothing useful
+				self.assertEqual(bold_label.text, brand_name) 
+				self.assertTrue(brand_name in self.driver.title)
+				#self.assertTrue(ad_text in self.driver.page_source) 
+				count -= 1 
+		except Exception as e:
+			self.driver.save_screenshot(os.getcwd() + os.path.sep + unittest.TestCase.id(self).split(".")[2] + ".png") 			
+			self.fail(e)
 
-	@unittest.skip("")
+
+
+	#@unittest.skip("")
 	def test_scenario2(self) :
-		count = 3 
-		while (count > 0):
-			self.driver.get("https://www.moat.com/")
-			E_text = [int(e.text.split()[0]) for e in self.driver.find_elements_by_css_selector("div.recently_seen_ads li.featured-agencies h4")]		
-			#print (E_text)
-			#E_text.append(35)
-			self.assertFalse(any(e >= 30 for e in E_text))
-			count -= 1 #decrement 
+		"2. Verify that the “Recently Seen Ads” are no more than half an hour old."
+		try:
+			count = 5
+			while (count > 0):
+				self.driver.get("https://www.moat.com/")
+				E_text = [int(e.text.split()[0]) for e in self.driver.find_elements_by_css_selector("div.recently_seen_ads li.featured-agencies h4")]		
+				#print (E_text)
+				#E_text.append(35)
+				self.assertFalse(any(e >= 30 for e in E_text))
+				count -= 1 #decrement 
+		except Exception as e:
+			self.driver.save_screenshot(os.getcwd() + os.path.sep + unittest.TestCase.id(self).split(".")[2] + ".png") 			
+			self.fail(e)		
 
-	@unittest.skip("")
+
+	#@unittest.skip("")
 	def test_scenario3(self) :
-		#L = ["Blick", "Trump Hotels", "Bob Dylan"]
-		L = ["Blick"]
-		for search_str in L:
+		"""3. Verify the ad count on the search result page is correct, 
+		even if there aremore than 100 ads in the result set."""
+
+		try:
 			self.driver.get("https://www.moat.com/")
 			searchBar = self.driver.find_element_by_id("pro-landing-search-box")
-			searchBar.send_keys(search_str)
+			
+			search_list = ["Pabst Blue Ribbon", "KLM", "Bob Dylan", "New Yorker"]
+			searchBar.send_keys(random.choice(search_list))
 			searchBar.submit()
-			#self.driver.refresh() #refreshes to whatever driver.current_url is set to
-			#validate next page
-			time.sleep(5) #0 creatives is diplayed without sleep
-			#wait = WebDriverWait(self.driver, 10)
-			#wait.until(EC.title_is(search_str + " Creatives | Moat")) 
-			print (self.driver.title)
-			print (self.driver.current_url)
-
-			counter = int(self.driver.find_element_by_css_selector("div.creative-count span.header-text").get_property('textContent').split()[0])
-			print (counter)
 			
-			try:
-				while (True):
-					self.driver.find_element_by_css_selector("a.er-load-more").click()
-					time.sleep(5) #what's the alternate solution, using waits perhaps
-			except (ElementNotInteractableException, ElementNotVisibleException) as e:
-				pass
-			
-			ads = self.driver.find_elements_by_css_selector("div.masonry-container > div") #
-			print (len(ads))
-			self.assertTrue(len(ads) == counter, "Ad count is: {},label displays: {}".format(len(ads), counter))
+			time.sleep(5) #0 creatives, another AJAX issue 
+			#print (self.driver.title)
+			#print (self.driver.current_url)
 
+			num_creatives = (self.driver.find_element_by_css_selector("div.creative-count span.header-text").get_property('textContent').split()[0])
+			num_creatives = re.sub('[,]', '',num_creatives) #strip out the comma
+			#print (num_creatives)
+
+			while (True):
+				try:
+					self.driver.find_element_by_css_selector("div.full-screen-load-more-container.visible")	
+				except Exception as e:
+					break 	
+				self.driver.find_element_by_css_selector("a.er-load-more").click()
+				time.sleep(5) #wait for ajax to load thumbnails (not long enough for larger load)
+		
+
+			ads = self.driver.find_elements_by_css_selector("div.masonry-container > div") 
+			self.assertEqual(len(ads), num_creatives, "Ad count is: {},label displays: {}".format(len(ads), int(num_creatives)))
+		except Exception as e:
+			self.driver.save_screenshot(os.getcwd() + os.path.sep + unittest.TestCase.id(self).split(".")[2] + ".png") 			
+			self.fail(e)		
+
+
+
+	#@unittest.skip("")
 	def test_scenario4(self) :
-		self.driver.get("https://www.moat.com/")
-		searchBar = self.driver.find_element_by_id("pro-landing-search-box")
-		searchBar.send_keys("Hotel")
-		searchBar.submit()
-		
-		ad = self.driver.find_elements_by_css_selector("div.masonry-container > div")[0]
+		"""4. Verify the “Share this Ad” feature"""
 
-		'''
-		signup_btn = self.driver.find_element_by_css_selector("a.signup-button")
-		ActionChains(self.driver).context_click(signup_btn).perform()
-		time.sleep(5)
-		self.driver.save_screenshot("/Users/shriamin/Development/test_scripts/selenium/moat/ss.png")
-		'''
+		try:
+			self.driver.get("https://www.moat.com/")
+			searchBar = self.driver.find_element_by_id("pro-landing-search-box")
+			searchBar.send_keys("Canon") #fix
+			searchBar.submit()
+			self.driver.find_elements_by_css_selector("div.masonry-container > div")[0].click()
+			self.driver.find_element_by_link_text("Share").click()
+			time.sleep(1)
+			#print(len(self.driver.window_handles))
+			
+			share_popup_txt = self.driver.find_element_by_css_selector("div.popup div.popup-header span.header-text").text
+			self.assertEqual(share_popup_txt, "Share")
+			
+			share_popup_close_btn = self.driver.find_element_by_css_selector("div.popup div.popup-header span.close-button img.close-popup-icon")
 
-		actions = ActionChains(self.driver)
-		actions.move_to_element(ad)
-		actions.perform()
-		self.driver.save_screenshot("/Users/shriamin/Development/test_scripts/selenium/moat/ss.png")
-		
+			share_popup_url_label_txt = self.driver.find_element_by_css_selector("div.popup div.popup-body span.url-label").text
+			self.assertEqual(share_popup_url_label_txt, "Shareable URL")	
 
-if __name__ == '__main__':
+			self.driver.find_element_by_css_selector("div.popup div.popup-body img.url-share-icon")
+			share_popup_url_input_box = self.driver.find_element_by_css_selector("div.popup div.popup-body div.url input.url-field")
+			share_url = share_popup_url_input_box.get_attribute("value")
+			static_url = "https://moat.com/creative/548af492583c784247090faef99de0cb?region=US" #fix
+			self.assertEqual(static_url, share_url) #static test
+
+
+			copy_btn = self.driver.find_element_by_css_selector("div.popup div.popup-body div.button-div button.copy-button")
+			self.assertEqual(copy_btn.text, "Copy Link")
+			copy_btn.click()
+			copy_url_msg = self.driver.find_element_by_css_selector("div.popup div.popup-body div.button-div span.url-message").text
+			self.assertEqual(copy_url_msg, "URL copied to clipboard!")
+			
+			self.driver.get(share_url) #open share url
+			self.assertEqual(self.driver.current_url, share_url)			
+		except Exception as e:
+			self.driver.save_screenshot(os.getcwd() + os.path.sep + unittest.TestCase.id(self).split(".")[2] + ".png") 			
+			self.fail(e)			
+
+
+
+if __name__ == '__main__':    
 	unittest.main()
