11011f605c68ca0f5426a71597e877eb47b3be6f
Shri Amin <shri.amin@gmail.com>
2017-06-29 12:00:04 -0400

    soduke update

--- python/dict1.py
+++ python/dict1.py
@@ -3,44 +3,43 @@
 ab = {  'Swaroop'   : 'swaroop@swaroopch.com',
         'Larry'     : 'larry@wall.org',
         'Matsumoto' : 'matz@ruby-lang.org',
-        'Spammer'   : 'spammer@hotmail.com'
-    }
-
+        'Spammer'   : 'spammer@hotmail.com' }
 print "Swaroop's address is", ab['Swaroop']
-
 # Deleting a key-value pair
 del ab['Spammer']
-
-print '\nThere are {} contacts in the address-book\n'.format(len(ab))
-
+print 'There are {} contacts in the address-book'.format(len(ab))
 for name, address in ab.items():
     print "Contact {} at {}".format(name, address)
-
 for name, address in ab.items():
     print "Contact", name, "at",  address
-
 for items in ab.items():
     print items[0], items[1]
 
 # Adding a key-value pair
 ab['Guido'] = 'guido@python.org'
-ab['dickhead'] = (1, 2)
-
+ab['cd'] = (1, 2)
 if 'Guido' in ab:
     print "\nGuido's address is {}", ab['Guido']
-
-if 'dickhead' in ab:
-    print "\n\n\n%s%s is dickhead's addy" % ab['dickhead']
-
+if 'cd' in ab:
+    print "%s%s is cd's addy\n" % ab['cd']
 
 
-k1 = ("a", "b", "c", "d") #
-v1 = ["foo", "bar"]
 
+k1 = ("a", "b", "c", "d") #the key can be a tuple
+v1 = ["foo", "bar"] 
 dict = {k1: v1}
-
 for items in dict.items():
-	print items[0], items[1]
+	print items[0], items[1], "\n"
+
+
+
+import operator
+x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+sorted_key = sorted(x.items(), key=operator.itemgetter(0))
+sorted_val = sorted(x.items(), key=operator.itemgetter(1))
+print (sorted_key)
+print (sorted_val)
+
 
 
 
--- python/soduku.py
+++ python/soduku.py
@@ -13,7 +13,86 @@ unitlist = ([cross(rows, c) for c in cols] +
 print(unitlist)
 units = dict((s, [u for u in unitlist if s in u]) 
              for s in squares)
-print (units)
+import pprint
+
+pprint.pprint (units)
 peers = dict((s, set(sum(units[s],[]))-set([s]))
              for s in squares)
+pprint.pprint (peers)
 
+
+
+def parse_grid(grid):
+    """Convert grid to a dict of possible values, {square: digits}, or
+    return False if a contradiction is detected."""
+    ## To start, every square can be any digit; then assign values from the grid.
+    values = dict((s, digits) for s in squares)
+    for s,d in grid_values(grid).items():
+        if d in digits and not assign(values, s, d):
+            return False ## (Fail if we can't assign d to square s.)
+    return values
+
+def grid_values(grid):
+    "Convert grid into a dict of {square: char} with '0' or '.' for empties."
+    chars = [c for c in grid if c in digits or c in '0.']
+    assert len(chars) == 81
+    initial_values = dict(zip(squares, chars))
+    return initial_values
+
+def assign(values, s, d):
+    """Eliminate all the other values (except d) from values[s] and propagate.
+    Return values, except return False if a contradiction is detected."""
+    other_values = values[s].replace(d, '')
+    if all(eliminate(values, s, d2) for d2 in other_values):
+        return values
+    else:
+        return False
+
+def eliminate(values, s, d):
+    """Eliminate d from values[s]; propagate when values or places <= 2.
+    Return values, except return False if a contradiction is detected."""
+    if d not in values[s]:
+        return values ## Already eliminated
+    values[s] = values[s].replace(d,'')
+    ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+    if len(values[s]) == 0:
+	return False ## Contradiction: removed last value
+    elif len(values[s]) == 1:
+        d2 = values[s]
+        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
+            return False
+    ## (2) If a unit u is reduced to only one place for a value d, then put it there.
+    for u in units[s]:
+	dplaces = [s for s in u if d in values[s]]
+	if len(dplaces) == 0:
+	    return False ## Contradiction: no place for this value
+	elif len(dplaces) == 1:
+	    # d can only be in one place in unit; assign it there
+            if not assign(values, dplaces[0], d):
+                return False
+    return values
+
+grid = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
+parse_grid(grid)
+
+
+
+import unittest
+class soduku_test(unittest.TestCase):
+	def test(self):
+	    "A set of unit tests."
+	    assert len(squares) == 81
+	    assert len(unitlist) == 27
+	    assert all(len(units[s]) == 3 for s in squares)
+	    assert all(len(peers[s]) == 20 for s in squares)
+	    assert units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+	                           ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+	                           ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
+	    assert peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
+	                               'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
+	                               'A1', 'A3', 'B1', 'B3'])
+	    print 'All tests pass.'
+
+
+if __name__ == '__main__':
+	unittest.main()
