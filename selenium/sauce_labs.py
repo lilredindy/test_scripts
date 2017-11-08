23e684becaf687591a5e9e9925ecbb0a41790550
shri amin <shri.amin@gmail.com>
2017-11-08 18:12:35 -0500

    sauce

--- selenium/sauce_labs.py
+++ selenium/sauce_labs.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
-sauce_url = "http://shriamin:abf4b3e5-cfab-4a79-824c-08a4d4d843f9@ondemand.saucelabs.com:80/wd/hub"
+#sauce_url = "http://shriamin:abf4b3e5-cfab-4a79-824c-08a4d4d843f9@ondemand.saucelabs.com:80/wd/hub"
+sauce_url = "http://lilredindy:6cd6bec2-262a-40e6-8097-2cb684c3f8b6@ondemand.saucelabs.com:80/wd/hub"
 
 desired_capabilities = {
     'platform': "Mac OS X 10.9",
@@ -10,8 +11,8 @@ desired_capabilities = {
 
 driver = webdriver.Remote(desired_capabilities=desired_capabilities,
                           command_executor=sauce_url)
-driver.implicitly_wait(10)
+driver.implicitly_wait(5)
 
 driver.get('http://google.com')
-assert "Goole" in driver.title
+assert "Google" in driver.title
 driver.quit()
